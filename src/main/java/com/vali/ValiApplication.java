3a76adaed7fedf6a098e063328a91a69ae100eff
ray <>
2018-06-09 00:28:51 +0900

    初期設定ファイルを追加

---
+++ src/main/java/com/vali/ValiApplication.java
@@ -0,0 +1,12 @@
+package com.vali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValiApplication.class, args);
+	}
+}
