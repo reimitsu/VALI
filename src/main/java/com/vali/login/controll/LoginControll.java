5f5e4765859a0e1e2eb36a47f8cf507bd5850490
ray <>
2018-06-09 13:03:56 +0900

    ログインURLを変更

--- src/main/java/com/vali/login/controll/LoginControll.java
+++ src/main/java/com/vali/login/controll/LoginControll.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginControll {
 
-    @RequestMapping("/login")
+    @RequestMapping("/")
     public String login() {
         return "ログイン画面は現在未実装です。";
     }
