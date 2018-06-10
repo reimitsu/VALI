599369187454c76a3771ea5a91691dad79cbc80d
ray <>
2018-06-10 23:22:23 +0900

    ログイン画面を調整

--- src/dist/sql/create/create_user_mst.sql
+++ src/dist/sql/create/create_user_mst.sql
@@ -8,5 +8,6 @@ CREATE
 		user_id VARCHAR(15) PRIMARY KEY,
 		user_name VARCHAR(15) NOT NULL,
 		password VARCHAR(60) NOT NULL,
-		del_flg VARCHAR(1) NOT NULL
+		del_flg VARCHAR(1) NOT NULL DEFAULT '0',
+		manage_flg VARCHAR(1) NOT NULL DEFAULT '0'
 	);
